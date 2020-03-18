e9979db93405f7b6f6483111cc85995a7485b83c
Roman <mom47@yandex.ru>
2020-03-18 16:53:15 +0300

    Lesson 11 - OOP inheritance.

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -1,33 +1,29 @@
 package mypackage;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Car car1 = new Car();
-        car1.color = "Black";
-        car1.length = 5000;
-        car1.height = 2000;
-        car1.width = 2000;
+        Cat cat = new Cat();
+        Dog dog = new Dog();
 
-        car1.addWeight(50);
-        car1.drive(120);
+        cat.name = "Vasya";
+        dog.name = "Barsik";
 
-        car1.addWeight(700);
-        car1.drive(100);
+        cat.color = "White";
+        dog.color = "Black";
 
-        Car car2 = new Car();
-        car2.color = "White";
-        Car car3 = new Car();
-        car3.color = "Red";
+        cat.walk("Backyard");
+        dog.walk("Park");
 
-        car2.drive(100);
-        car3.drive(150);
+        cat.sleep();
+        dog.sleep();
 
-        Car car4 = new Car("Black");
-        System.out.println("Car color is: " + car4.color);
+        cat.feed("Milk");
+        dog.sit();
 
-        Car car5 = new Car("Red", 2100, 2000, 5100);
-        System.out.println(car5.color + " " + car5.height + " " + car5.width + " " + car5.length);
+        System.out.println("Cat name is: " + cat.name);
+        System.out.println("Dog name is: " + dog.name);
 
     }
 }
