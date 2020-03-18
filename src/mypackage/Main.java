e4de4f5ba2fe0eb6f1ba5876deacf4d1d68c3c61
Roman <mom47@yandex.ru>
2020-03-18 14:31:50 +0300

    Firest lesson, Output>"Hello world" from console.

---
+++ src/mypackage/Main.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World! ");
+        System.out.println("I'ts my first Programme! ");
+        System.out.println("Good bye!");
+    }
+}
