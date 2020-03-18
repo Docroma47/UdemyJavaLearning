687a876da30041359023d2f239e196dc6f28e4be
Roman <mom47@yandex.ru>
2020-03-18 16:25:48 +0300

    Lesson 7 - Collections "Set". Plus "iterator".

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -1,72 +1,31 @@
 package mypackage;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        set.add(10);
+        set.add(11);
+        set.add(12);
+        set.add(11);
 
-        list.add(5);
-        list.add(10);
-        list.add(9);
+        System.out.println(set.size());
 
-        int a = list.get(2);
+        set.remove(11);
 
-        System.out.println(list.get(1));
-        System.out.println(a);
-
-        list.set(2, 100);
-
-        System.out.println(list.get(2));
-        System.out.println();
-        System.out.println(list.get(1));
-        list.remove(1);
-        System.out.println(list.get(1));
-        System.out.println();
-        System.out.println(list.get(1));
-        System.out.println();
-        list.add(9);
-        System.out.println(list.size());
-
-//        list.clear();
-        System.out.println(list.size());
-
-        List<Integer> list2 = new ArrayList<>();
-        list2.add(45);
-        list2.add(56);
-
-        System.out.println();
-        System.out.println(list.size());
-        list.addAll(list2);
-        System.out.println(list.size());
-
-        list.removeAll(list2);
-        System.out.println(list.size());
-
-        System.out.println();
-        System.out.println(list.isEmpty());
-//        list.clear();
-        System.out.println(list.isEmpty());
-
-        System.out.println();
-        System.out.println(list.contains(100));
-        System.out.println(list.contains(987));
-
-        list.addAll(list2);
-        System.out.println();
-        System.out.println(list.containsAll(list2));
-        list.removeAll(list2);
-        System.out.println(list.containsAll(list2));
-
-        Iterator<Integer> iterator = list.iterator();
-//        System.out.println(iterator.next());
-//        System.out.println(iterator.next());
-//        System.out.println(iterator.hasNext());
-        System.out.println();
-        
+        Iterator<Integer> iterator = set.iterator();
         while (iterator.hasNext()) System.out.println(iterator.next());
+
+//        set.clear();
+        System.out.println(set.size());
+
+        System.out.println();
+        System.out.println(set.contains(10));
+        System.out.println(set.contains(11));
+
+        System.out.println();
+        for (int i: set) System.out.println(i);
     }
 
 }
