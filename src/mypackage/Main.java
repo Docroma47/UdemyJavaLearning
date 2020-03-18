4027cbc6e5406b9ba21208088af705d4af961da6
Roman <mom47@yandex.ru>
2020-03-18 16:35:46 +0300

    Lesson 7 - Collections "Map". Plus "iterator" and "Set".

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -4,28 +4,44 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> set = new HashSet<>();
-        set.add(10);
-        set.add(11);
-        set.add(12);
-        set.add(11);
 
-        System.out.println(set.size());
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Black");
+        map.put(2, "White");
+        map.put(3, "Yellow");
+        map.put(10, "Green");
 
-        set.remove(11);
+        System.out.println(map.get(1));
+        System.out.println(map.get(10));
 
-        Iterator<Integer> iterator = set.iterator();
-        while (iterator.hasNext()) System.out.println(iterator.next());
-
-//        set.clear();
-        System.out.println(set.size());
+        System.out.println(map.size());
+//        map.clear();
+        System.out.println(map.size());
 
         System.out.println();
-        System.out.println(set.contains(10));
-        System.out.println(set.contains(11));
+        System.out.println(map.containsKey(90));
+        System.out.println(map.containsKey(10));
+        System.out.println(map.containsValue("White"));
+        System.out.println(map.containsValue("Grey"));
 
         System.out.println();
-        for (int i: set) System.out.println(i);
+        Set<Integer> keys = map.keySet();
+        Iterator<Integer> iterator = keys.iterator();
+        while (iterator.hasNext()) System.out.println(map.get(iterator.next()));
+
+        System.out.println();
+        System.out.println(map.get(2));
+        map.put(2, "Yellow");
+        System.out.println(map.get(2));
+
+        Map<String, String> map2 = new HashMap<>();
+
+        map2.put("VASYAN!", "Adidas");
+        map2.put("Gora", "Puma");
+
+        System.out.println();
+        System.out.println(map2.get("VASYAN!"));
+        System.out.println(map2.get("Gora"));
     }
 
 }
