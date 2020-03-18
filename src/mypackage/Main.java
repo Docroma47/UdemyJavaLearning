ab948e4cb214f0812605d78d3a5a918f6d644d46
Roman <mom47@yandex.ru>
2020-03-18 16:40:50 +0300

    Lesson 7 - Overload voids.

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -1,33 +1,24 @@
 package mypackage;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        int a = 13;
-        int b = 3;
-        int sum;
+        int result;
 
-        sum = getSum(a, b);
-        System.out.println(sum);
-        sum = getSum(10, 150);
-        System.out.println(sum);
-        System.out.println(getSum(2, 11));
-
-        showSum(10, 51, 77);
-        showSum(5, 12, 14);
-
-        saySmth();
-        saySmth();
-        saySmth();
+        result = getSum(1, 10);
+        System.out.println(result);
+        result = getSum(5, 100, 66);
+        System.out.println(result);
 
         sayHello("Petya");
-        String name = "Vasya";
-        sayHello(name);
+        sayHello("Vasya", "Vanya");
+        sayHello();
 
-        showSumToPerson("Vanya", 15, 11, 33);
+        int[] array = {1, 22, 52, 34, 16};
 
-        boolean result = showSum(1 , 22, 333);
-        System.out.println(result);
+        int sum = getSum(array, "Oleg");
+        System.out.println(sum + 100);
 
     }
 
@@ -37,18 +28,22 @@ public class Main {
         return sum;
     }
 
-    static boolean showSum(int x, int y, int z){
-        int sum = x + y + z;
-        System.out.println("Sum is: " + sum);
-        return true;
+    static int getSum(int x, int y, int z){
+        int sum;
+        sum = x + y + z;
+        return sum;
     }
 
-    static void saySmth(){
-        System.out.println();
-        System.out.println("Hello!");
-        System.out.println("I'm running!");
-        System.out.println("You see me!");
+    static int getSum(int[] array, String name){
+        int sum = 0;
+        for (int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        System.out.println("Hi " + name + "!");
+        System.out.println("Your sum is: " + sum);
+        return sum;
     }
+
 
     static void sayHello(String name){
         System.out.println();
@@ -57,11 +52,19 @@ public class Main {
         System.out.println("Nice to meet you!");
     }
 
-    static void showSumToPerson(String name, int a, int b, int c){
-        System.out.println("Start of program!");
-        sayHello(name);
-        showSum(a, b, c);
-        System.out.println("End of program!");
+    static void sayHello(String name1, String name2){
+        System.out.println();
+        System.out.println("Hello " + name1 +"!");
+        System.out.println("Hello " + name2 +"!");
+        System.out.println("I'm your program!");
+        System.out.println("Nice to meet you!");
+    }
+
+    static void sayHello(){
+        System.out.println();
+        System.out.println("Hello");
+        System.out.println("I'm your program!");
+        System.out.println("Nice to meet you!");
     }
 
 }
