145abc766dd04b7053eb42edfa3981070fa8820a
Roman <mom47@yandex.ru>
2020-03-18 16:39:16 +0300

    Lesson 7 - Voids.

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -1,47 +1,67 @@
 package mypackage;
 
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
 
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "Black");
-        map.put(2, "White");
-        map.put(3, "Yellow");
-        map.put(10, "Green");
+        int a = 13;
+        int b = 3;
+        int sum;
 
-        System.out.println(map.get(1));
-        System.out.println(map.get(10));
+        sum = getSum(a, b);
+        System.out.println(sum);
+        sum = getSum(10, 150);
+        System.out.println(sum);
+        System.out.println(getSum(2, 11));
 
-        System.out.println(map.size());
-//        map.clear();
-        System.out.println(map.size());
+        showSum(10, 51, 77);
+        showSum(5, 12, 14);
 
+        saySmth();
+        saySmth();
+        saySmth();
+
+        sayHello("Petya");
+        String name = "Vasya";
+        sayHello(name);
+
+        showSumToPerson("Vanya", 15, 11, 33);
+
+        boolean result = showSum(1 , 22, 333);
+        System.out.println(result);
+
+    }
+
+    static int getSum(int x, int y){
+        int sum;
+        sum = x + y;
+        return sum;
+    }
+
+    static boolean showSum(int x, int y, int z){
+        int sum = x + y + z;
+        System.out.println("Sum is: " + sum);
+        return true;
+    }
+
+    static void saySmth(){
         System.out.println();
-        System.out.println(map.containsKey(90));
-        System.out.println(map.containsKey(10));
-        System.out.println(map.containsValue("White"));
-        System.out.println(map.containsValue("Grey"));
+        System.out.println("Hello!");
+        System.out.println("I'm running!");
+        System.out.println("You see me!");
+    }
 
+    static void sayHello(String name){
         System.out.println();
-        Set<Integer> keys = map.keySet();
-        Iterator<Integer> iterator = keys.iterator();
-        while (iterator.hasNext()) System.out.println(map.get(iterator.next()));
+        System.out.println("Hello " + name +"!");
+        System.out.println("I'm your program!");
+        System.out.println("Nice to meet you!");
+    }
 
-        System.out.println();
-        System.out.println(map.get(2));
-        map.put(2, "Yellow");
-        System.out.println(map.get(2));
-
-        Map<String, String> map2 = new HashMap<>();
-
-        map2.put("VASYAN!", "Adidas");
-        map2.put("Gora", "Puma");
-
-        System.out.println();
-        System.out.println(map2.get("VASYAN!"));
-        System.out.println(map2.get("Gora"));
+    static void showSumToPerson(String name, int a, int b, int c){
+        System.out.println("Start of program!");
+        sayHello(name);
+        showSum(a, b, c);
+        System.out.println("End of program!");
     }
 
 }
